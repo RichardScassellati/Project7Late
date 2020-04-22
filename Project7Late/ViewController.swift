471e9db43b1e261a41fe1add5404c03a48dd0d90
Richard Scassellati <rn.scassellati@setonhill.edu>
2020-04-21 23:40:09 -0400

    Initial Commit

---
+++ Project7Late/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7Late
+//
+//  Created by Richard Scassellati on 4/21/20.
+//  Copyright © 2020 Smecko Geck Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
